e37ce277c271c693c929425f8658d54e7ecb6d5a
TheophileMoreau <theophile.freelance@gmail.com>
2025-12-11 23:29:47 +0100

    chore: log stylesheet addition

--- public/js/addStylesheet.js
+++ public/js/addStylesheet.js
@@ -1,4 +1,5 @@
 export function addStylsheet (stylesheet) {
+    console.log(`Adding ${stylesheet} stylesheet !`);
     var elemement = document.getElementById('variable-stylesheet');
     elemement.href = './styles/' + stylesheet + '.css';
 }
